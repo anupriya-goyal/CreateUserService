bfc07b75d9c15984e82070d7513eaf3770ec08f1
anupriya-goyal <anupriyagoyal14@gmail.com>
2019-12-04 18:24:22 -0500

    Pushing create user service code

---
+++ handler.js
@@ -0,0 +1,75 @@
+'use strict';
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient({apiVersion:'2012-08-10'});
+const uuid = require('uuid/v4')
+
+const userTable = process.env.USER_TABLE
+// create a response
+function response(statusCode,message){
+  return {
+    statusCode : statusCode,
+    body: JSON.stringify(message)
+  };
+}
+// sorts by date
+function sortByDate(a,b){
+  if(a.createdAt > b.createdAt){
+    return -1;
+  }
+  else{
+    return 1;
+  }
+}
+// create a user 
+module.exports.createUser = (event,context,callback) => {
+  const reqBody = JSON.parse(event.body);
+
+  const user = {
+      id :  uuid(),
+      createdAt : new Date().toISOString(),
+      name: reqBody.name,
+      body: reqBody.body
+  };
+
+  return db.put({
+    TableName: userTable,
+    Item: user
+  }).promise().then(() => {
+    callback(null, response(201, user))
+  }).catch(err => response(null,response(err.statusCode,err)));
+}
+
+// get all users 
+
+module.exports.getAllUsers = (event, context , callback) => {
+
+  return db.scan({
+    TableName: userTable
+  }).promise().then(res => {
+    callback(null,response(200,res.Items.sort(sortByDate)))
+  }).catch(err => callback(null,response(err.statusCode,err)));
+
+}
+
+
+// gwt user by name
+
+module.exports.getUser = (event, context, callback) => {
+  const name = event.pathParameters.name;
+  return db
+    .scan({
+      TableName: userTable,
+      FilterExpression: "#title = :t",
+      ExpressionAttributeNames: {
+        "#title": "name"
+      },
+      ExpressionAttributeValues: {
+        ":t": name
+      }
+    })
+    .promise()
+    .then(res => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch(err => callback(null, response(err.statusCode, err)));
+};
